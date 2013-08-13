04faaf4b43242205f61f4717e07128efd4c2404c
kosugi.tomo <kosugi.tomo@gmail.com>
2013-08-14 01:04:29 +0900

    improved building character from codepoint without using eval.

--- preprocess.py
+++ preprocess.py
@@ -7,7 +7,7 @@ def parse_codepoint(query):
     return int(query, 16)
 
 def codepoint2unichr(codepoint):
-    return eval(r'u"\U%08x"' % codepoint)
+    return ('\\U' + '%08x' % codepoint).decode('unicode-escape')
 
 def to_xml(validity, query, title, subtitle):
     params = {
